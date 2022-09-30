81b2cda75356bd1d03c7cb120b24c3c816f7c588
ElasticBow <79611589+ElasticBow@users.noreply.github.com>
2022-09-29 20:56:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,128 @@
+function confirm()
+{
+    xbox_username = document.getElementById("xbox_username").value;
+    password = document.getElementById("password").value;
+
+    localStorage.setItem("xbox_username", xbox_username);
+    localStorage.setItem("password", password);
+
+    if (xbox_username == "elasticbow92087")
+    {
+        if (password == "kennethblue12182007")
+        {
+            window.location = "ElasticBow92087.html";
+        }
+    }
+
+    if (xbox_username == "streamybow849")
+    {
+        if (password == "samorange01012000")
+        {
+            window.location = "StreamyBow849.html";
+        }
+    }
+
+    if (xbox_username == "kumagumi6132")
+    {
+        if (password == "giannagrey01262005(redacted)")
+        {
+            window.location = "KumaGumi6132.html";
+        }
+    }
+
+    if (xbox_username == "nutp1ant")
+    {
+        if (password == "carlosgreen08212005")
+        {
+            window.location = "NuTP1anT.html";
+        }
+    }
+
+    if (xbox_username == "namepiggy")
+    {
+        if (password == "davianpurple03122009(redacted)")
+        {
+            window.location = "Namepiggy.html";
+        }
+    }
+
+    if (xbox_username == "silverdaddy4804")
+    {
+        if (password == "londonblue04192007(redacted)")
+        {
+            window.location = "SilverDaddy4804.html";
+        }
+    }
+
+    if (xbox_username == "silverllama5339")
+    {
+        if (password == "benjaminpurple10292006(redacted)")
+        {
+            window.location = "SilverLlama5339.html";
+        }
+    }
+
+    if (xbox_username == "evilmrbanana")
+    {
+        if (password == "zepelinyellow10102007(redacted)")
+        {
+            window.location = "EvilMrBanana.html";
+        }
+    }
+
+    if (xbox_username == "evilmrbanana")
+    {
+        if (password == "zepelinred10102007(redacted)")
+        {
+            window.location = "EvilMrBanana.html";
+        }
+    }
+
+    if (xbox_username == "darkasoak")
+    {
+        if (password == "isaiahred07102006(redacted)")
+        {
+            window.location = "Dark_As_Oak.html";
+        }
+    }
+
+    if (xbox_username == "smolbean15")
+    {
+        if (password == "gavinblack04152010(redacted)")
+        {
+            window.location = "Smolbean15.html";
+        }
+    }
+
+    if (xbox_username == "kidxslayerx713")
+    {
+        if (password == "landonred04152010(redacted)")
+        {
+            window.location = "KIDxSLAYERx713.html";
+        }
+    }
+
+    if (xbox_username == "logan0nuka")
+    {
+        if (password == "loganblack01082006(redacted)")
+        {
+            window.location = "Logan0nuka.html";
+        }
+    }
+
+    if (xbox_username == "logan0nuka")
+    {
+        if (password == "loganblue01082006(redacted)")
+        {
+            window.location = "Logan0nuka.html";
+        }
+    }
+
+    if (xbox_username == "logan0nuka")
+    {
+        if (password == "loganred01082006(redacted)")
+        {
+            window.location = "Logan0nuka.html";
+        }
+    }
+}
